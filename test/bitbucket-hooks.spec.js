d5c421726edb9d306e12bc1483c888da83fde5cb
Taras Labiak <kissarat@gmail.com>
2020-02-15 22:24:31 +0200

    + getHookChanges absent test

--- test/bitbucket-hooks.spec.js
+++ test/bitbucket-hooks.spec.js
@@ -2,9 +2,12 @@ const { deepStrictEqual } = require('assert');
 const { getHookChanges } = require('../src/bitbucket-hooks');
 const PushEvent = require('./bitbucket-push-event');
 
+const testHookChanges = (expected, branches) => deepStrictEqual(
+    expected,
+    getHookChanges(PushEvent, 'equestrian', branches)
+);
+
 describe('bitbucket hooks', () => {
-    it('getHookChanges', () => deepStrictEqual(
-        ['bitbucket'],
-        getHookChanges(PushEvent, 'equestrian', ['master', 'bitbucket'])
-    ))
+    it('getHookChanges single', () => testHookChanges(['bitbucket'], ['master', 'bitbucket']));
+    it('getHookChanges absent', () => testHookChanges([], ['production', 'test']));
 });
